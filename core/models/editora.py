850a208875adb9807d66419ae8996e5bc8f8102e
nicoleabrand <nicoleabrand13@gmail.com>
2025-06-02 16:48:02 -0300

    fix: livro

--- core/models/editora.py
+++ core/models/editora.py
@@ -4,6 +4,8 @@ from django.db import models
 class Editora(models.Model):
     nome = models.CharField(max_length=135)
     site = models.CharField(max_length=230, blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.nome
