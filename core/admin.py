e6e5607ab2b2a71810547d7786d36ee84c1b8dc1
nicoleabrand <nicoleabrand13@gmail.com>
2025-06-03 14:21:38 -0300

    feat: customização do Admin

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,7 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+from core.models import Autor, Categoria, Editora, Livro, User
 
 from core import models
 
@@ -50,9 +51,73 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name', 'passage_id')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            },
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+        (_('Groups'), {'fields': ('groups',)}),
+        (_('User Permissions'), {'fields': ('user_permissions',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                ),
+            },
+        ),
+    )
 
-admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Categoria)
-admin.site.register(models.Editora)
-admin.site.register(models.Autor)
-admin.site.register(models.Livro)
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cidade')
+    search_fields = ('nome', 'email', 'cidade')
+    list_filter = ('nome', 'email', 'cidade')
+    ordering = ('nome', 'email', 'cidade')
+    list_per_page = 10
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25
