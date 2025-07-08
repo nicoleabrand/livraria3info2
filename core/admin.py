21e122cbcfa9f0766e737cd24c221b0c96a61390
nicoleabrand <nicoleabrand13@gmail.com>
2025-07-08 14:15:25 -0300

    feat: uso de TabularInline e StackedInline no Admin para Itens da Compra

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, User, Compra, ItensCompra
+from core.models import Autor, Categoria, Compra, Editora, ItensCompra, Livro, User
 
 
 class UserAdmin(BaseUserAdmin):
@@ -128,13 +128,17 @@ class LivroAdmin(admin.ModelAdmin):
     ordering = ('titulo', 'editora', 'categoria')
     list_per_page = 25
 
-@admin.register(ItensCompra)
-class ItensCompraAdmin(admin.ModelAdmin):
-    list_per_page = 10
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+    extra = 1  # Quantidade de itens adicionais
+
 
 @admin.register(Compra)
 class CompraAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'status')
+    search_fields = ('usuario', 'status')
+    list_filter = ('usuario', 'status')
     ordering = ('usuario', 'status')
     list_per_page = 10
+    inlines = [ItensCompraInline]
 
--- core/models/compra.py
+++ core/models/compra.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-from .user import User
 from .livro import Livro
+from .user import User
+
 
 class Compra(models.Model):
     class StatusCompra(models.IntegerChoices):
@@ -11,10 +12,10 @@ class Compra(models.Model):
         ENTREGUE = 4, 'Entregue'
 
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name='compras')
-    status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
-       
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+
+
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name='itens')
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name='+')
     quantidade = models.IntegerField(default=1)
-
